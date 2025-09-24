c50cdd2be5798e99646461368c8462cb616b9673
Rzn39 <ericv79312@outlook.com>
2025-09-24 17:40:47 -0600

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,25 @@
+import os
+
+
+def variables():
+    main = []
+    vector = []
+    sn = []
+
+
+def input():
+    x = 0
+    y = input('enter input')
+
+    return y
+
+
+def interface():
+    if y = 'main':
+        print(main)
+    if y = 'vector':
+        print(vector)
+    if y = sn
+        print(sn)
+
+
